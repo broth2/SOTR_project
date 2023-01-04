eb2600f1002c3446219047978b68faea115d3945
rt <joaocastanheira@ua.pt>
2023-01-04 13:28:08 +0000

    serial

--- projCodeExamples/serialTest.c
+++ projCodeExamples/serialTest.c
@@ -23,15 +23,20 @@
 
 
 // C library headers
-#include <stdio.h>
-#include <string.h>
+
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
 
 // Linux headers
 #include <fcntl.h> // Contains file controls like O_RDWR
 #include <errno.h> // Error integer and strerror() function
 #include <termios.h> // Contains POSIX terminal control definitions
 #include <unistd.h> // write(), read(), close()
+
 
 int main() {
     // Open the serial port. Change device path as needed (currently set to an standard FTDI USB-UART cable type device)
@@ -41,10 +46,10 @@ int main() {
     struct termios tty;
 
     // Read in existing settings, and handle any error
-    if(tcgetattr(serial_port, &tty) != 0) {
-      printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
-      return 1;
-    }
+    // if(tcgetattr(serial_port, &tty) != 0) {
+    //   printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+    //   return 1;
+    // }
 
     tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
     tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
@@ -66,108 +71,66 @@ int main() {
     // tty.c_oflag &= ~OXTABS; // Prevent conversion of tabs to spaces (NOT PRESENT ON LINUX)
     // tty.c_oflag &= ~ONOEOT; // Prevent removal of C-d chars (0x004) in output (NOT PRESENT ON LINUX)
 
-    tty.c_cc[VTIME] = 0;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
-    tty.c_cc[VMIN] = 16;
+    tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+    tty.c_cc[VMIN] = 0;
 
     // Set in/out baud rate to be 115200
     cfsetispeed(&tty, B115200);
     cfsetospeed(&tty, B115200);
 
     // Save tty settings, also checking for error
-    if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
-      printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
-      return 1;
+    // if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
+    //   printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+    //   return 1;
+    // }
+
+
+    DIR *folder;
+    struct dirent *entry;
+    int files = 0;
+
+    folder = opendir("/home/rt/SOTR_project/projCodeExamples");
+    if(folder == NULL)
+    {
+        perror("Unable to read directory");
+        return(1);
     }
+	struct timespec ts;
+
+	ts.tv_sec = 8;
+    ts.tv_nsec = (800 % 1000) * 1000000;
+
+    while( (entry=readdir(folder)) )
+    {
+ 
+        files++;
+        int len = strlen (entry->d_name);
+        if ((len >= 4) && strcmp (&(entry->d_name[len - 4]), ".raw") == 0){
+            char filename[10];
+            strcpy(filename, (entry->d_name));
+			//printf("%s\n", filename);
+			int rows, columns;
+			rows = 128;
+			columns = 128;  
+			uint8_t temp[128*128];
+            uint8_t read_buf[60];
+
+            FILE* imgRaw;
+			char ch;
+            imgRaw = fopen(filename,"rb");
+			for (int i = 0; i < 1 ; ++i) { 
+		 	   fread(&temp, sizeof(uint8_t), 128*128, imgRaw);
+               //printf("temp[%d] : %u",i,temp);
+			   write(serial_port, &temp, sizeof(temp));
+               sleep(3);
 
 
-
-
-    // Write to serial port
-    //unsigned char msg[] = { 'H', 'e', 'l', 'l', 'o', '\r' };
-
-
-    uint8_t msg[] = {
-      0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 
-      0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
-      0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F,
-      0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F,
-      0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F,
-      0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0x5B, 0x5C, 0x5D, 0x5E, 0x5F};
-
-/*
-      0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F,
-      0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A, 0x7B, 0x7C, 0x7D, 0x7E, 0x7F,
-      0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F,
-      0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98, 0x99, 0x9A, 0x9B, 0x9C, 0x9D, 0x9E, 0x9F,
-      0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF,
-      0xB0, 0xB1, 0xB2, 0xB3, 0xB4, 0xB5, 0xB6, 0xB7, 0xB8, 0xB9, 0xBA, 0xBB, 0xBC, 0xBD, 0xBE, 0xBF,
-      0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF,
-      0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0xD5, 0xD6, 0xD7, 0xD8, 0xD9, 0xDA, 0xDB, 0xDC, 0xDD, 0xDE, 0xDF,
-      0xE0, 0xE1, 0xE2, 0xE3, 0xE4, 0xE5, 0xE6, 0xE7, 0xE8, 0xE9, 0xEA, 0xEB, 0xEC, 0xED, 0xEE, 0xEF,
-      0xF0, 0xF1, 0xF2, 0xF3, 0xF4, 0xF5, 0xF6, 0xF7, 0xF8, 0xF9, 0xFA, 0xFB, 0xFC, 0xFD
-    };*/
-
-
-
-    // ########## TODO: Read img1.raw to img99.raw ##########
-    /*
-    int msg[16384];
-    for (int i = 0; i < 16384; i++) {
-      msg[i] = i;
-    }
-    */
-
-    
-
-    write(serial_port, msg, sizeof(msg));
-
-    // Allocate memory for read buffer, set size according to your needs
-    uint8_t read_buf [64];
-
-    // Normally you wouldn't do this memset() call, but since we will just receive
-    // ASCII data for this example, we'll set everything to 0 so we can
-    // call printf() easily.
-                    //memset(&read_buf, '\0', sizeof(read_buf));
-
-    /* Loop forever and rpint output 
-    * Stop with CTRL-C */
-    while(1) {
-
-                      //memset(&read_buf, '\0', sizeof(read_buf));
-
-        // Read bytes. The behaviour of read() (e.g. does it block?,
-        // how long does it block for?) depends on the configuration
-        // settings above, specifically VMIN and VTIME
-        int num_bytes = read(serial_port, &read_buf, sizeof(read_buf));
-
-        // n is the number of bytes read. n may be 0 if no bytes were received, and can also be -1 to signal an error.
-        if (num_bytes < 0) {
-            printf("Error reading: %s", strerror(errno));
-            return 1;
+			}
+			
+			fclose(imgRaw);
+          
         }
-
-
-        //if (num_bytes == -1) {
-          //printf("", num_bytes, read_buf);
-        //} else {
-          // Here we assume we received ASCII data, but you might be sending raw bytes (in that case, don't try and
-          // print it to the screen like this!)
-
-                        //printf("\nRead %i bytes. Received message: %02x", num_bytes, read_buf);
-                        
-                        
-
-                        // print the received message in raw hex
-
-
-        printf("\nRead %i bytes. Received message: ", num_bytes);
-        for (int i = 0; i < num_bytes; i++) {
-          printf("%02x ", read_buf[i]);
-        }
-
-
-                        
-        //}       
+        
     }
 
 	close(serial_port);
