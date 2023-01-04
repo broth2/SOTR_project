236056110d655082189fb4aa5c14f2eb0a0ae929
Fahla <nunofahla@ua.pt>
2023-01-04 08:47:24 +0000

    .

--- src/ipa.h
+++ src/ipa.h
@@ -1,11 +1,11 @@
 #ifndef IPA
 #define IPA
 #include <stdint.h>
-#define IMGWIDTH 128
+#define IMGWIDTH 32
 
 int guideLineSearch(uint8_t imageBuf[IMGWIDTH][IMGWIDTH], int16_t *pos, float *angle);
 int obstCount(uint8_t imageBuf[IMGWIDTH][IMGWIDTH]);
 int csaObjects(uint8_t imageBuf[IMGWIDTH][IMGWIDTH]);
-void test();
+//void test();
 
 #endif
